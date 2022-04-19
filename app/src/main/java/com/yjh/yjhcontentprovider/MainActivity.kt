f0baa8987c5ed36365e2247c61377b231b3bb104
Flameyjh <1171286601@qq.com>
2022-04-19 21:08:17 +0800

    创建自己的 ContentProvider

--- app/src/main/java/com/yjh/yjhcontentprovider/MainActivity.kt
+++ app/src/main/java/com/yjh/yjhcontentprovider/MainActivity.kt
@@ -10,7 +10,7 @@ import kotlin.Exception
 /*
 * 创建自己的 ContentProvider
 * 使用之前的 yjhDatabaseTest工程，把里面的数据提供给外部
-* 实现重点：DatabaseProvider
+* 实现重点：DatabaseProvider文件
 * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
